b7d6926adb3b84cf91ee4a45e9eecb599eccc1a1
TomBombadiI <78550692+TomBombadiI@users.noreply.github.com>
2025-01-14 16:59:47 +0700

    change public to protected $uri

--- core/Application.php
+++ core/Application.php
@@ -5,11 +5,12 @@ namespace PHPFramework;
 class Application
 {
 
-    public string $uri;
+    protected string $uri;
     public Request $request;
     public Response $response;
-    
-    public function __construct() {
+
+    public function __construct()
+    {
         $this->uri = $_SERVER['QUERY_STRING'];
         $this->request = new Request($this->uri);
         $this->response = new Response();
