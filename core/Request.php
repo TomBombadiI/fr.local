cf8801fd1ccad7820696b57393e4d5ee768f3859
TomBombadiI <78550692+TomBombadiI@users.noreply.github.com>
2025-01-16 11:14:41 +0700

    add some methods to Request

--- core/Request.php
+++ core/Request.php
@@ -14,11 +14,46 @@ class Request
 
     public function getPath(): string
     {
-        return $this->uri;
+        return $this->getUriWithoutQuery();
     }
 
     public function getMethod(): string
     {
         return strtoupper($_SERVER['REQUEST_METHOD']);
+    }
+
+    public function isGet(): bool
+    {
+        return $this->getMethod() === 'GET';
+    }
+
+    public function isPost(): bool
+    {
+        return $this->getMethod() === 'POST';
+    }
+
+    public function isAjax(): bool
+    {
+        return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest';
+    }
+
+    public function get($key, $default = null): ?string
+    {
+        return $_GET[$key] ?? $default;
+    }
+
+    public function post($key, $default = null): ?string
+    {
+        return $_POST[$key] ?? $default;
+    }
+
+    protected function getUriWithoutQuery(): string
+    {
+        if ($this->uri) {
+            $params = explode('?', $this->uri);
+            return trim($params[0], '/');
+        }
+
+        return '';
     }
 }
